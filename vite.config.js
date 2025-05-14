3bf46ca2d48b4c295993db63b83d988eb2010e94
Allie <yawnzzn183@gmail.com>
2025-05-15 00:31:02 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./', //新增這行放上Github
-  plugins: [vue()],
+  base:'./',
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
 })
